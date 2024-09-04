dbc52d47130b2b27fcb9a4ac9d263713cfaa7123
fatemeh-akbari-humber <Fatemeh.akbari.mashhadi@gmail.com>
2024-09-03 21:04:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace finalexam2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ValueType<int> collection = new ValueType<int>();
+
+          
+            collection.AddItem(11);
+            collection.AddItem(5);
+            collection.AddItem(25);
+
+            
+            Console.WriteLine("Item at index 1: " + collection.GetItem(1));
+
+            var sortedItems = collection.GetSortedDescending();
+            Console.WriteLine("Sorted items in descending order: " + string.Join(", ", sortedItems));
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ ValueType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace finalexam2
+{
+   public class ValueType<T> where T : struct, IComparable<T>
+    {
+
+        private List<T> _items;
+
+        public ValueType()
+        {
+            _items = new List<T>();
+        }
+
+   
+        public void AddItem(T item)
+        {
+            _items.Add(item);
+        }
+
+  
+        public T GetItem(int index)
+        {
+            if (index < 0 || index >= _items.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+            }
+            return _items[index];
+        }
+
+        public List<T> GetSortedDescending()
+        {
+            return _items.OrderByDescending(item => item).ToList();
+        }
+    }
+}
